3b3a9777924b62a43b252162b81f29d6a7a0f4bf
Sırdaş <srdas.04@hotmail.com>
2025-08-13 13:22:33 +0300

    Update lua_mesaj_silme.lua

--- lua_mesaj_silme.lua
+++ lua_mesaj_silme.lua
@@ -1,29 +1,39 @@
+-- KEYS[1] = "baglantida_mi:{alici}"
+-- KEYS[2] = "offline_mesajlar:{alici}" (HASH)
+-- KEYS[3] = "offline_mesajlar_idx:{alici}" (ZSET)
+-- ARGV[1] = mesajId
+-- ARGV[2] = msgpack(bytes)
+-- ARGV[3] = timestamp (ms)
+-- ARGV[4] = max_keep (ör. 1000) ya da "" (boş) => yok
+-- ARGV[5] = ttl_ms (ör. 2592000000 = 30 gün) ya da "" => yok
 
+local online = redis.call('GET', KEYS[1])
+if online == 'true' then
+  return 0
+end
 
--- remove_incoming_read_on_exit.lua
--- KEYS[1] = "sohbet:{chat_id}"
--- ARGV[1] = user_id
+redis.call('HSET', KEYS[2], ARGV[1], ARGV[2])
+redis.call('ZADD', KEYS[3], ARGV[3], ARGV[1])
 
-local list_key = KEYS[1]
-local user_id  = ARGV[1]
-
-local ids = redis.call('LRANGE', list_key, 0, -1)
-local removed = {}
-
-for _, mid in ipairs(ids) do
-  local hkey = 'mesaj:' .. mid
-  local alici = redis.call('HGET', hkey, 'alici')
-  if alici == user_id then
-    local readset = 'mesaj:' .. mid .. ':read_by'
-    if redis.call('SISMEMBER', readset, user_id) == 1 then
-      -- listeden düş
-      redis.call('LREM', list_key, 0, mid)
-      -- mesaj metaverisi ve okuyanlar setini tamamen sil
-      redis.call('DEL', hkey)
-      redis.call('DEL', readset)
-      table.insert(removed, mid)
+-- trim
+if ARGV[4] ~= '' then
+  local maxk = tonumber(ARGV[4])
+  local cnt = redis.call('ZCARD', KEYS[3])
+  if cnt > maxk then
+    local over = cnt - maxk
+    local olds = redis.call('ZRANGE', KEYS[3], 0, over - 1)
+    for _, mid in ipairs(olds) do
+      redis.call('HDEL', KEYS[2], mid)
     end
+    redis.call('ZREMRANGEBYRANK', KEYS[3], 0, over - 1)
   end
 end
 
-return removed
+-- TTL
+if ARGV[5] ~= '' then
+  local ttl = tonumber(ARGV[5])
+  redis.call('PEXPIRE', KEYS[2], ttl)
+  redis.call('PEXPIRE', KEYS[3], ttl)
+end
+
+return 1
